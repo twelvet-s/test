b2bf3daf0ca590716b9af277b2cf6fcbdb32d66b
TwelveT <2471835953@qq.com>
2023-02-20 10:18:48 +0800

    hash分库分表，二次扩容

--- data/twelvet-fast.sql
+++ data/twelvet-fast.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : twelvet-mysql
+ Source Server         : localhost
  Source Server Type    : MySQL
  Source Server Version : 50726 (5.7.26-log)
- Source Host           : twelvet-mysql:3306
+ Source Host           : dev:3306
  Source Schema         : twelvet-fast
 
  Target Server Type    : MySQL
  Target Server Version : 50726 (5.7.26-log)
  File Encoding         : 65001
 
- Date: 12/02/2023 14:45:15
+ Date: 20/02/2023 10:18:02
 */
 
 SET NAMES utf8mb4;
@@ -22,39 +22,59 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_login_info_0`;
 CREATE TABLE `sys_login_info_0`  (
-  `id` bigint(20) NOT NULL COMMENT '访问ID',
-  `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '用户账号',
-  `ipaddr` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '登录IP地址',
-  `status` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '0' COMMENT '登录状态（0登录成功 1登录失败 2成功退出）',
-  `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '提示信息',
-  `access_time` datetime NULL DEFAULT NULL COMMENT '访问时间',
-  `dept_id` bigint(20) NULL DEFAULT NULL COMMENT '部门ID',
-  PRIMARY KEY (`id`) USING BTREE
+                                     `id` bigint(20) NOT NULL COMMENT '访问ID',
+                                     `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '用户账号',
+                                     `ipaddr` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '登录IP地址',
+                                     `status` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '0' COMMENT '登录状态（0登录成功 1登录失败 2成功退出）',
+                                     `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '提示信息',
+                                     `access_time` datetime NULL DEFAULT NULL COMMENT '访问时间',
+                                     `dept_id` bigint(20) NULL DEFAULT NULL COMMENT '部门ID',
+                                     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci COMMENT = '系统访问记录' ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of sys_login_info_0
--- ----------------------------
-INSERT INTO `sys_login_info_0` VALUES (1, 'user1', '', '0', '', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for sys_login_info_1
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_login_info_1`;
 CREATE TABLE `sys_login_info_1`  (
-  `id` bigint(20) NOT NULL COMMENT '访问ID',
-  `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '用户账号',
-  `ipaddr` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '登录IP地址',
-  `status` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '0' COMMENT '登录状态（0登录成功 1登录失败 2成功退出）',
-  `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '提示信息',
-  `access_time` datetime NULL DEFAULT NULL COMMENT '访问时间',
-  `dept_id` bigint(20) NULL DEFAULT NULL COMMENT '部门ID',
-  PRIMARY KEY (`id`) USING BTREE
+                                     `id` bigint(20) NOT NULL COMMENT '访问ID',
+                                     `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '用户账号',
+                                     `ipaddr` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '登录IP地址',
+                                     `status` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '0' COMMENT '登录状态（0登录成功 1登录失败 2成功退出）',
+                                     `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '提示信息',
+                                     `access_time` datetime NULL DEFAULT NULL COMMENT '访问时间',
+                                     `dept_id` bigint(20) NULL DEFAULT NULL COMMENT '部门ID',
+                                     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci COMMENT = '系统访问记录' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Records of sys_login_info_1
+-- Table structure for sys_login_info_2
 -- ----------------------------
-INSERT INTO `sys_login_info_1` VALUES (2, 'user2', '', '0', '', NULL, NULL);
+DROP TABLE IF EXISTS `sys_login_info_2`;
+CREATE TABLE `sys_login_info_2`  (
+                                     `id` bigint(20) NOT NULL COMMENT '访问ID',
+                                     `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '用户账号',
+                                     `ipaddr` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '登录IP地址',
+                                     `status` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '0' COMMENT '登录状态（0登录成功 1登录失败 2成功退出）',
+                                     `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '提示信息',
+                                     `access_time` datetime NULL DEFAULT NULL COMMENT '访问时间',
+                                     `dept_id` bigint(20) NULL DEFAULT NULL COMMENT '部门ID',
+                                     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci COMMENT = '系统访问记录' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sys_login_info_3
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_login_info_3`;
+CREATE TABLE `sys_login_info_3`  (
+                                     `id` bigint(20) NOT NULL COMMENT '访问ID',
+                                     `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '用户账号',
+                                     `ipaddr` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '登录IP地址',
+                                     `status` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '0' COMMENT '登录状态（0登录成功 1登录失败 2成功退出）',
+                                     `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT '' COMMENT '提示信息',
+                                     `access_time` datetime NULL DEFAULT NULL COMMENT '访问时间',
+                                     `dept_id` bigint(20) NULL DEFAULT NULL COMMENT '部门ID',
+                                     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci COMMENT = '系统访问记录' ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
--- src/main/java/com/twelvet/domain/SysLoginInfo.java
+++ src/main/java/com/twelvet/domain/SysLoginInfo.java
@@ -1,5 +1,6 @@
 package com.twelvet.domain;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -10,6 +11,7 @@ import java.util.Date;
  */
 public class SysLoginInfo implements Serializable {
 
+	@Serial
 	private static final long serialVersionUID = 1L;
 
 	/**
